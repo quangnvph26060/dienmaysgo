41984455926b3d674b9c50bbe69b6e93450f24fa
datdev <dnguyentien145@gmail.com>
2024-12-05 14:08:02 +0700

    update code

---
+++ public/frontends/assets/js/custom.js
@@ -0,0 +1,26 @@
+document.querySelector(".cart-link").addEventListener("click", function (e) {
+    e.preventDefault();
+    const popup = document.querySelector(".mfp-content");
+
+    // Hiển thị popup với animation
+
+    setTimeout(() => {
+        popup.classList.add("open");
+        document.querySelector(".off-canvas#cart-popup").style.display = "block"; // Đảm bảo nó hiện trước
+
+        document.querySelector(".mfp-bg").style.display = "block";
+    }, 300); // Thêm một chút thời gian để áp dụng hiệu ứng trượt
+});
+
+// Tùy chọn: Đóng popup
+document.querySelector(".mfp-container").addEventListener("click", function () {
+    const popup = document.querySelector(".mfp-content");
+
+    console.log(popup);
+
+    popup.classList.remove("open");
+    setTimeout(() => {
+        document.querySelector(".off-canvas#cart-popup").style.display = "none"; // Ẩn sau khi hiệu ứng hoàn tất
+        document.querySelector(".mfp-bg").style.display = "none";
+    }, 300);
+});
