64706287d8eb4fe77ce23cff411cc3d0fd0f3471
dev_phongpt <phamtuanphongisme@gmail.com>
2024-12-11 17:25:33 +0700

    update cart

--- resources/views/frontends/layouts/partials/popup-cart.blade.php
+++ resources/views/frontends/layouts/partials/popup-cart.blade.php
@@ -12,41 +12,42 @@
 
                     <div class="widget_shopping_cart_content">
                         <ul class="woocommerce-mini-cart cart_list product_list_widget">
-
                             @foreach (Cart::instance('shopping')->content() as $cart)
                                 <li class="woocommerce-mini-cart-item mini_cart_item">
-                                    <a class="remove remove_from_cart_button" data-row-id="{{ $cart->id }}" data-product_id="{{ $cart->id }}">×</a>
+                                    <a class="remove remove_from_cart_button" data-row-id="{{ $cart->id }}"
+                                        data-product_id="{{ $cart->id }}">×</a>
                                     <a href="https://dienmaysgo.com/may-phat-dien-chay-xang-elemax-sv2800/">
                                         <img width="300" height="300"
-                                            src="{{ asset('storage/'.$cart->options->image) }}"
-                                            data-src="{{ asset('storage/'.$cart->options->image) }}"
+                                            src="{{ asset('storage/' . $cart->options->image) }}"
+                                            data-src="{{ asset('storage/' . $cart->options->image) }}"
                                             class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail lazy-load-active"
                                             alt="" decoding="async" fetchpriority="high"
-                                            sizes="(max-width: 300px) 100vw, 300px" />{{$cart->name}}
+                                            sizes="(max-width: 300px) 100vw, 300px" />{{ $cart->name }}
                                     </a>
-                                    <span class="quantity">{{$cart->qty}} ×
-                                        <span class="woocommerce-Price-amount amount"><bdi>{{ formatAmount($cart->price)}}<span
+                                    <span class="quantity">{{ $cart->qty }} ×
+                                        <span class="woocommerce-Price-amount amount"><bdi>{{ formatAmount($cart->price) }}<span
                                                     class="woocommerce-Price-currencySymbol">₫</span></bdi></span></span>
-                                        <p class="sum_total" style="display: none">{{ formatAmount($cart->subtotal)}}</p>
+                                    <p class="sum_total" style="display: none">{{ formatAmount($cart->subtotal) }}</p>
                                 </li>
                             @endforeach
                         </ul>
 
                         <p class="woocommerce-mini-cart__total total">
                             <strong>Tổng cộng:</strong>
-                            <span class="woocommerce-Price-amount amount"><bdi><span class="total_cart">{{ number_format((float) str_replace([',', '.'], ['', '.'], Cart::instance('shopping')->subTotal()), 0, ',', '.') }}
-
-
-                            </span><span
-                                        class="woocommerce-Price-currencySymbol">₫</span></bdi></span>
+                            <span class="woocommerce-Price-amount amount"><bdi><span
+                                        class="total_cart">{{ number_format((float) str_replace([',', '.'], ['', '.'], Cart::instance('shopping')->subTotal()), 0, ',', '.') }}
+                                    </span><span class="woocommerce-Price-currencySymbol">₫</span></bdi></span>
                         </p>
 
-                        <p class="woocommerce-mini-cart__buttons buttons">
-                            <a href="{{route('carts.list')}}" class="button wc-forward">Xem giỏ hàng</a><a
-                                href="https://dienmaysgo.com/thanh-toan/"
-                                class="button checkout wc-forward">Thanh toán</a>
-                        </p>
+                        @if (Cart::instance('shopping')->count() > 0)
+                            <p class="woocommerce-mini-cart__buttons buttons" id="cart-links">
+                                <a href="{{ route('carts.list') }}" class="button wc-forward">Xem giỏ hàng</a>
+                                <a href="https://dienmaysgo.com/thanh-toan/" class="button checkout wc-forward">Thanh
+                                    toán</a>
+                            </p>
+                        @endif
                     </div>
+
                     <div class="cart-sidebar-content relative"></div>
                 </div>
             </div>
@@ -65,58 +66,57 @@
 
 <style>
     .remove.loading {
-    position: relative;
-    pointer-events: none; /* Ngăn người dùng nhấn thêm lần nữa */
-}
-
-.remove.loading::after {
-    content: "";
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 20px;
-    height: 20px;
-    border: 2px solid rgba(0, 0, 0, 0.2);
-    border-top: 2px solid #000; /* Màu của vòng xoay */
-    border-radius: 50%;
-    animation: spin 0.6s linear infinite;
-}
-
-@keyframes spin {
-    from {
-        transform: translate(-50%, -50%) rotate(0deg);
+        position: relative;
+        pointer-events: none;
+        /* Ngăn người dùng nhấn thêm lần nữa */
     }
-    to {
-        transform: translate(-50%, -50%) rotate(360deg);
-    }
-}
 
+    .remove.loading::after {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 20px;
+        height: 20px;
+        border: 2px solid rgba(0, 0, 0, 0.2);
+        border-top: 2px solid #000;
+        /* Màu của vòng xoay */
+        border-radius: 50%;
+        animation: spin 0.6s linear infinite;
+    }
+
+    @keyframes spin {
+        from {
+            transform: translate(-50%, -50%) rotate(0deg);
+        }
+
+        to {
+            transform: translate(-50%, -50%) rotate(360deg);
+        }
+    }
 </style>
 
 <script>
-
-
-
     function updateTotalPrice() {
-                let prices = [];
+        let prices = [];
 
-                // Lấy giá trị từ các phần tử có class 'quantity-price-sum'
-                document.querySelectorAll(".sum_total").forEach(element => {
-                    prices.push(element.textContent.trim());
-                });
+        // Lấy giá trị từ các phần tử có class 'quantity-price-sum'
+        document.querySelectorAll(".sum_total").forEach(element => {
+            prices.push(element.textContent.trim());
+        });
 
-                // Chuyển đổi các giá trị này thành số và tính tổng
-                let numericValues = prices.map(value => {
-                    return parseInt(value.replace(/[^\d]/g, ''));
-                });
+        // Chuyển đổi các giá trị này thành số và tính tổng
+        let numericValues = prices.map(value => {
+            return parseInt(value.replace(/[^\d]/g, ''));
+        });
 
-                let total = numericValues.reduce((sum, current) => sum + current, 0);
+        let total = numericValues.reduce((sum, current) => sum + current, 0);
 
-                // Cập nhật giá trị tổng vào các phần tử có class 'price-product-total'
-                document.querySelectorAll('.total_cart').forEach(element => {
-                    element.innerHTML = `${formatCurrency(total)}`;
-                });
+        // Cập nhật giá trị tổng vào các phần tử có class 'price-product-total'
+        document.querySelectorAll('.total_cart').forEach(element => {
+            element.innerHTML = `${formatCurrency(total)}`;
+        });
     }
     document.querySelectorAll(".remove").forEach(button => {
         button.addEventListener("click", function(event) {
@@ -158,9 +158,8 @@
     });
 
     function formatCurrency(amount) {
-            const formattedAmount = amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        const formattedAmount = amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-            return formattedAmount ;
+        return formattedAmount;
     }
-
 </script>
