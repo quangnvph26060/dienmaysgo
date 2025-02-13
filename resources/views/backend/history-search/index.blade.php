b640bc3ac7c064c63ee83ea745cfa574e9d584ea
datdev <dnguyentien145@gmail.com>
2025-02-13 15:35:23 +0700

    update code

--- resources/views/backend/history-search/index.blade.php
+++ resources/views/backend/history-search/index.blade.php
@@ -29,7 +29,7 @@
         $(document).ready(function() {
             const api = '{{ route('admin.marketing.history-search') }}'
 
-            dataTables(api, columns, 'HistorySearch', true, false, false, 'keywords')
+            dataTables(api, columns, 'historySearch', true, false, false, 'keywords')
 
             handleDestroy()
 
